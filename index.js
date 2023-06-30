def5cc265b5f67e559eb0d55dfc4b91615e6804f
Manu <manu.angot@hotmail.fr>
2023-06-30 10:51:21 +0200

    Set up basic files serverExpress

---
+++ index.js
@@ -0,0 +1,4 @@
+import { startServer } from "./server";
+import { app } from "./app";
+
+startServer(app);
---
+++ server.js
@@ -0,0 +1,15 @@
+import http from "http";
+
+const startServer = (app) => {
+    const server = http.createServer(app);
+    const port = process.env.port || 3000;
+
+    server.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+
+    });
+
+};
+
+export { startServer };
+
