5a81311c56ec20aca19ecbc59acbb71a9cd3c207
Gaston59400 <doroff59@gmail.com>
2023-07-06 11:14:59 +0200

    Merci Bryan

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
+import router from "./components/user/user.routes";
 
 const app = express();
+
+app.use('/users', router)
 
 app.get('/', (req, res) => {
     res.send("Hello world !");
