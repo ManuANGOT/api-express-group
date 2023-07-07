641b08dc2ffb2f83a1a3cd8ce5bbd5b46d2e996b
Gaston59400 <doroff59@gmail.com>
2023-07-07 11:13:03 +0200

    Update p3p Req List Users

--- src/components/user/user.routes.js
+++ src/components/user/user.routes.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import { getUsers, createUsers, updateUsers, deleteUsers } from './user.controller';
+import {getUsers} from "./user.controller"
 
 const router = express.Router();
 
 router.get('/', getUsers);
 
-router.post('/', createUsers);
+// router.post('/', createUsers);
 
-router.put('/:id', updateUsers);
+// router.put('/:id', updateUsers);
 
-router.delete('/:id', deleteUsers);
+// router.delete('/:id', deleteUsers);
 
 export default router;
